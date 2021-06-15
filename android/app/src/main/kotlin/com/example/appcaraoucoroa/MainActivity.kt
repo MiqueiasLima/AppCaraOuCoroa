1b36937db772d59cb866026e622c4ac9e01919b7
Miqueias Lima <miqueiaslima@outlook.com>
2021-06-14 21:48:04 -0300

    Criando a Classe do App

---
+++ android/app/src/main/kotlin/com/example/appcaraoucoroa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appcaraoucoroa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:appcaraoucoroa/CaraOuCoroa.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter Aplication",
+    debugShowCheckedModeBanner: false,
+    home: CaraOuCoroa()
+  ));
+}
+
