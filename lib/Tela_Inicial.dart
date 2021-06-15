a6725f09eeaf5ae3c1802ece7c9e0c8a903ac553
Miqueias Lima <miqueiaslima@outlook.com>
2021-06-14 22:12:35 -0300

    Finalizando Layout da Tela Inicial

--- lib/Tela_Inicial.dart
+++ lib/Tela_Inicial.dart
@@ -12,7 +12,18 @@ class _Tela_InicialState extends State<Tela_Inicial> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: ,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 10),
+          child: Image.asset("imagens/logo.png"),),
+
+          Padding(padding: EdgeInsets.only(top: 10),
+            child: Image.asset("imagens/botao_jogar.png"),)
+
+        ],
+      ) ,
     );
   }
 }
